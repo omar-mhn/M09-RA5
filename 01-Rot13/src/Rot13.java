6db1423436d559151b2e52cc2d9f15a2aff3309f
omar <omar@>
2025-09-26 19:31:57 +0200

    correció de desxiratRot13

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -1,36 +1,55 @@
 
 public class Rot13 {
+    final static char[] MINUSCULES = "aàábcçdeèéfghiìíïjklmnñoòópqrstuùúüvwxyz".toCharArray();
+    final static char[] MAJUSCULES = "AÀÁBCÇDEÈÉFGHIÌÍÏJKLMNÑOÒÓPQRSTUÙÚÜVWXYZ".toCharArray();
 
     public static String xifraRot13(String cadena) {
-        char[] minuscules = "aàábcçdeèéfghiìíïjklmnñoòópqrstuùúüvwxyz".toCharArray();
-        char[] majuscules = "AÀÁBCÇDEÈÉFGHIÌÍÏJKLMNÑOÒÓPQRSTUÙÚÜVWXYZ".toCharArray();
+       
         // ici j'ai transformè le texte en tableau 
         char [] chars = cadena.toCharArray();
         for (int j = 0; j < chars.length; j++) {
             // crée une variable temporaire c qui contient ce caractère pour pouvoir le manipuler plus facilement dans la boucle
             char c = chars[j];
-            for (int i = 0; i < minuscules.length; i++) {
-                if ( c == minuscules[i]){
-                    chars[j] = minuscules[(i + 13) % minuscules.length]  ;
+            for (int i = 0; i < MINUSCULES .length; i++) {
+                if ( c == MINUSCULES [i]){
+                    chars[j] = MINUSCULES [(i + 13) % MINUSCULES .length]  ;
                     break;
                 }
-                if ( c == majuscules[i]){
-                    chars[j] = majuscules[(i + 13) % majuscules.length]  ;
+                if ( c == MAJUSCULES[i]){
+                    chars[j] = MAJUSCULES[(i + 13) % MAJUSCULES.length]  ;
                     break ;
                 }
             }
         }
         return new String(chars);
     }
-    // ici j'appelle une deuxieme fois la xifra pour faire un autre tour car c'est symétrique  le chiffrement et le déchiffrement sont identiques 
      public static String desxifraRot13(String cadena) {
         
-        return xifraRot13(cadena);
+          // ici j'ai transformè le texte en tableau 
+        char [] chars = cadena.toCharArray();
+        for (int j = 0; j < chars.length; j++) {
+            // crée une variable temporaire c qui contient ce caractère pour pouvoir le manipuler plus facilement dans la boucle
+            char c = chars[j];
+            for (int i = 0; i < MINUSCULES .length; i++) {
+                if ( c == MINUSCULES [i]){
+                    chars[j] = MINUSCULES[(i - 13) + (MINUSCULES.length % MINUSCULES.length)];
+
+                    break;
+                }
+                if ( c == MAJUSCULES[i]){
+                    chars[j] = MAJUSCULES[( (i - 13 + MAJUSCULES.length) % MAJUSCULES.length )]  ;
+                    break ;
+                }
+            }
+        }
+        return new String(chars);
     }
 
 public static void main(String[] args) {
     System.out.println(xifraRot13("ABC")); 
+    System.out.println(desxifraRot13("IÏJ"));
     System.out.println(xifraRot13("hola")); 
+    System.out.println(desxifraRot13("ówùi"));
     
 
 }
